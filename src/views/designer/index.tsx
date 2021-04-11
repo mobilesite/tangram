e8b1a22e1df99066327c678bc6b764ded78d7eaa
wumingliang <wumingliang@100tal.com>
2021-04-11 17:55:16 +0800

    feat: 添加在线将编辑器的代码进行Babel编译的功能

--- src/views/designer/index.tsx
+++ src/views/designer/index.tsx
@@ -2,12 +2,19 @@ import { defineComponent } from 'vue';
 import MonacoEditor from '../../components/monaco-editor';
 import './index.less';
 
+declare global {
+  interface Window {
+    Babel: any
+  }
+}
+
 export default defineComponent({
   name: 'Designer',
   props: {},
   render() {
     const onCodeChanged = (newCode: string) => {
-      console.log('newCode', newCode);
+      const compiledCode = window.Babel.transform(newCode, { presets: ['env'] }).code;
+      console.log('newCode', compiledCode);
     };
 
     const onInitialized = (editor: any) => {
@@ -18,7 +25,7 @@ export default defineComponent({
       <MonacoEditor
         width="100%"
         height={200}
-        code="function test() {\n  console.log(1111);\n}"
+        code="function test() {\n  const a = 123;\n  console.log(1111);\n}"
         onInitialized={onInitialized}
         onCodeChanged={onCodeChanged}
       ></MonacoEditor>
